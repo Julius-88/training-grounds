f7f98cbd5fde4c6280745fcbd7989f05d7efb3d1
Julius Alamarvdashti <julius.alamarvdashti@gmail.com>
2026-04-29 11:41:53 +0200

    Add user input practice in C#

---
+++ CSharpMasterclassProject/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// **Using Variable String**
+
+// Prints out whatever is inside the brackets
+//Console.WriteLine("Hello, Julius!");
+
+//string myFriendsName;
+//myFriendsName = "Jannick";
+
+//Console.WriteLine(myFriendsName);
+
+//myFriendsName = "Frank";
+//Console.WriteLine(myFriendsName);
+
+//// Takes the user input and stores it
+//Console.ReadLine();
+
+// **Understanding User Input and the Data Type String**
+
+Console.WriteLine("Enter something");
+
+// Variable with the name "userInput" and the data type "string"
+// Strings are used for texts
+string userInput = Console.ReadLine();
+Console.WriteLine("You have entered " + userInput);
+// Will wait until we press a key before closing the console
+Console.ReadKey();
